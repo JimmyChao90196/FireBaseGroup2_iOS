2574a51ed44c0dff3dd2b054a1a10835c49cf147
JimmyChao <myname90196@gmail.com>
2023-10-23 13:51:40 +0800

    Initial Commit

---
+++ FireBaseGroup2_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireBaseGroup2_iOS
+//
+//  Created by JimmyChao on 2023/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
